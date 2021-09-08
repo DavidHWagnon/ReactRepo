cd4b0f5c9f5d567b1f0dafda09363abc4120cec7
David H Wagnon <david.wagnon02@gmail.com>
2021-09-07 19:50:30 -0500

    Added styling to personal website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,54 @@
 import logo from './twitterPic.jpg';
 import twitchLogo from './img/twitchLogo.png';
+import twitterLogo from './img/twitterLogo.png';
+import githubLogo from './img/githubLogo.png';
+import linkedinLogo from './img/linkedinLogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to David Wagnon's personal website! Feel free to look around.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.twitch.tv/tymeitv"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="Twitch-logo" src={twitchLogo}></img>
-        </a>
+        <div className="transparent">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to David Wagnon's personal website! Feel free to look around.
+          </p>
+          <div className="social-row">
+            <a
+              className="App-link"
+              href="https://www.linkedin.com/in/david-wagnon-19036720b/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="social-logo" src={linkedinLogo}></img>
+            </a>
+            <a
+              className="App-link"
+              href="https://github.com/DavidHWagnon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="social-logo" src={githubLogo}></img>
+            </a>
+            <a
+              className="App-link"
+              href="https://twitter.com/Tymeitv"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="social-logo" src={twitterLogo}></img>
+            </a>
+            <a
+              className="App-link"
+              href="https://www.twitch.tv/tymeitv"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="social-logo" src={twitchLogo}></img>
+            </a>
+        </div>
+        </div>
       </header>
     </div>
   );
